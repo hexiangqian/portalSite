efacb822c034ebfaee322634fb602e4acdc24f3d
fanpei <fanpei1413@163.com>
2020-02-18 23:39:01 +0800

    修改注解描述，并更改注解类名

--- src/src/main/java/zy/news/web/zsys/bean/ExcutInterfaceDsrc.java
+++ src/src/main/java/zy/news/web/zsys/bean/ExcutInterfaceDsrc.java
@@ -3,14 +3,14 @@ package zy.news.web.zsys.bean;
 import java.lang.annotation.*;
 
 /**
- * 方法描述
+ * 执行接口描述
  *
  * @author fanpei
  */
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface ExcuteMethodDsrc {
+public @interface ExcutInterfaceDsrc {
 
     /**
      * 操作名称
