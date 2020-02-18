286455272ae54560d2a7981af5b1a89e6596fffe
fanpei <fanpei1413@163.com>
2020-02-18 23:44:46 +0800

    去除过时标记

--- src/src/main/java/zy/news/common/ValuesPage.java
+++ src/src/main/java/zy/news/common/ValuesPage.java
@@ -9,48 +9,48 @@ import java.util.List;
  * @author fanpei
  */
 public class ValuesPage {
-	private Object ext;// 额外
-	private Page page;
-	private List value;
+    private Object ext;// 额外
+    private Page page;
+    private List value;
 
 
-	public ValuesPage() {
-	}
+    public ValuesPage() {
+    }
 
-	public ValuesPage(Page page) {
-		this.page = page.copy();
-	}
+    public ValuesPage(Page page) {
+        this.page = page.copy();
+    }
 
-	public ValuesPage(List value, Page page) {
-		this.value = value;
-		this.page = page;
-	}
+    public ValuesPage(List value, Page page) {
+        this.value = value;
+        this.page = page;
+    }
 
-	public Page getPage() {
-		return page;
-	}
+    public Page getPage() {
+        return page;
+    }
 
-	public List getValue() {
-		return value;
-	}
+    public List getValue() {
+        return value;
+    }
 
-	public void setPage(Page page) {
-		this.page = page;
-	}
+    public void setPage(Page page) {
+        this.page = page;
+    }
 
-	public void setValue(List value) {
-		this.value = value;
-	}
+    public void setValue(List value) {
+        this.value = value;
+    }
 
-	public Object getExt() {
-		return ext;
-	}
+    public Object getExt() {
+        return ext;
+    }
 
-	public void setExt(Object ext) {
-		this.ext = ext;
-	}
+    public void setExt(Object ext) {
+        this.ext = ext;
+    }
 
-	public boolean listValueEmpty() {
-		return value == null || value.isEmpty();
-	}
+    public boolean listValueEmpty() {
+        return value == null || value.isEmpty();
+    }
 }
